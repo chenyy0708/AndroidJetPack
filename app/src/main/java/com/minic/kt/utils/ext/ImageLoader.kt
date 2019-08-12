3abae79b57dfb44196959b1b1c43bf5e3db2f1d0
chenyy <chenyangyi@boohee.com>
2019-08-12 10:24:36 +0800

    使用simple data预览数据

--- app/src/main/java/com/minic/kt/utils/ext/ImageLoader.kt
+++ app/src/main/java/com/minic/kt/utils/ext/ImageLoader.kt
@@ -1,6 +1,7 @@
 package com.minic.kt.utils.ext
 
 import android.widget.ImageView
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.transition.DrawableCrossFadeFactory
 import com.minic.kt.R
@@ -17,6 +18,7 @@ fun ImageView.loadImage(any: Any?, radius: Int = 0) {
     GlideApp.with(this.context)
             .load(any)
             .placeholder(R.mipmap.ic_launcher_round)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
             .error(R.mipmap.ic_launcher_round)
             .centerCrop()
 //            .transition(DrawableTransitionOptions.with(
